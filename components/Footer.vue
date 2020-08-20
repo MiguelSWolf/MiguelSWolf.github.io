d4288a011cff66a471aa387e20fd75b1af571d13
MiguelSWolf <soulmiguel@gmail.com>
2020-08-20 09:29:13 -0300

    cleaning the project

---
+++ components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer>
+    <div class="container">
+      Let's talk about stuff<br />
+      Linkedin CodePen Github Skype
+    </div>
+  </footer>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <header>
+    <div class="container">
+      just the header
+    </div>
+  </header>
+</template>
