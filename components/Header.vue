07d2947f90d98d89bd10a0cdf8da494a6c5fb902
MiguelSWolf <soulmiguel@gmail.com>
2020-09-18 15:22:34 -0300

    adjuste of resolutions with the container size

--- components/Header.vue
+++ components/Header.vue
@@ -8,6 +8,13 @@
         <!-- <a href="#">About</a>
         <a href="#">Work</a>
         <a href="#">Other</a> -->
+
+        <a target="_blank" href="https://www.linkedin.com/in/miguel-wolf/"
+          >Linkedin</a
+        >
+        <a target="_blank" href="https://codepen.io/hachaalma">CodePen</a>
+        <a target="_blank" href="https://github.com/MiguelSWolf">Github</a>
+        <a target="_blank" href="skype:soulmiguel@live.com">Skype</a>
       </nav>
     </div>
   </header>
