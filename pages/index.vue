9991a67010fb242288a57a3aa3c20fa251c4ec75
MiguelSWolf <soulmiguel@gmail.com>
2020-09-22 10:25:10 -0300

    fix the problems with columns

--- pages/index.vue
+++ pages/index.vue
@@ -9,8 +9,8 @@
         <div>
           <div class="box-content">
             <h3 class="content">
-              My name is Miguel Wolf, and I'm a front-end developer based in Brazil.
-              I've been working in this area for almost 10 years.
+              My name is Miguel Wolf, and I'm a front-end developer based in
+              Brazil. I've been working in this area for almost 10 years.
             </h3>
           </div>
         </div>
@@ -23,38 +23,49 @@
             Technology
           </h2>
           <div class="items">
-            <p class="item is-big">VueJS</p>
-            <p class="item">Vuex</p>
-            <p class="item">Vue Resources</p>
-            <p class="item">Axios</p>
-            <p class="item">Nuxt</p>
-            <p class="item">Nuxt</p>
+            <div class="group">
+              <p class="item is-big">VueJS</p>
+              <p class="item">Vuex</p>
+              <p class="item">Vue Resources</p>
+              <p class="item">Axios</p>
+              <p class="item">Next</p>
+              <p class="item">Nuxt</p>
+            </div>
 
-            <p class="item is-big">PHP</p>
-            <p class="item">Laravel</p>
-            <p class="item">CodeIgniter</p>
-            <p class="item">Wordpress</p>
+            <div class="group">
+              <p class="item is-big">PHP</p>
+              <p class="item">Laravel</p>
+              <p class="item">CodeIgniter</p>
+              <p class="item">Wordpress</p>
+            </div>
 
-            <p class="item is-big">Web</p>
-            <p class="item">HTML5</p>
-            <p class="item">CSS3</p>
-            <p class="item">SCSS</p>
-            <p class="item">Bulma.io</p>
-            <p class="item">Bootstrap</p>
-            <p class="item">JS Vanilla</p>
-            <p class="item">Gulp</p>
-            <p class="item">Webpack</p>
+            <div class="group">
+              <p class="item is-big">Web</p>
+              <p class="item">HTML5</p>
+              <p class="item">CSS3</p>
+              <p class="item">SCSS</p>
+              <p class="item">Bulma.io</p>
+              <p class="item">Bootstrap</p>
+              <p class="item">JS Vanilla</p>
+              <p class="item">Gulp</p>
+              <p class="item">Webpack</p>
+            </div>
 
-            <p class="item is-big">Database</p>
-            <p class="item">MySQL</p>
-            <p class="item">MariaDB</p>
-            <p class="item">SQLite</p>
-            <p class="item">PostgreSQL</p>
+            <div class="group">
+              <p class="item is-big">Database</p>
+              <p class="item">MySQL</p>
+              <p class="item">MariaDB</p>
+              <p class="item">SQLite</p>
+              <p class="item">PostgreSQL</p>
+              <p class="item">Firebase</p>
+            </div>
 
-            <p class="item is-big">Amazon AWS</p>
-            <p class="item">Amazon SES</p>
-            <p class="item">Amazon EC2</p>
-            <p class="item">Amazon E3</p>
+            <div class="group">
+              <p class="item is-big">Amazon AWS</p>
+              <p class="item">Amazon SES</p>
+              <p class="item">Amazon EC2</p>
+              <p class="item">Amazon E3</p>
+            </div>
           </div>
         </div>
       </div>
